04326cbf59d48fb11f17e7f75704942eaf9b332a
Владислав Персаев <perssskk@mail.ru>
2026-09-24 20:27:23 +0300

    с хвостиком

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -5,15 +5,14 @@
         static void Main(string[] args)
         {
             SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+            SinglyLinkedListWithTail singlyLinkedListWithTail = new SinglyLinkedListWithTail();
+            singlyLinkedListWithTail.PushFront(9);
+            singlyLinkedListWithTail.PushFront(4);
+            singlyLinkedListWithTail.PushFront(2);
 
-            //Node count = singlyLinkedList.FindByIndex(2);
-            singlyLinkedList.AddFirst(1);
-            singlyLinkedList.AddFirst(3);
-            singlyLinkedList.AddFirst(7);
-            singlyLinkedList.AddFirst(9);
+            Node node = new Node(4);
+            singlyLinkedListWithTail.AddAfter(node, 1);
 
-            Console.WriteLine("сам список:" + singlyLinkedList.Print());
-            Console.WriteLine(singlyLinkedList.FindByIndex(2).Value);
         }
 
 
@@ -95,6 +94,150 @@
                     currentIndex++;
                 }
                 return current;
+            }
+        }
+        class SinglyLinkedListWithTail
+        {
+            public Node Head;
+            public Node Tail;
+
+            private int count = 0; // количество элементов в спискпе
+
+            public int GetCount()
+            {
+                return count;
+            }
+
+            public string Print()
+            {
+                string result = "";
+                Node current = Head;
+                while (current != null)
+                {
+                    result += current.Value;
+                    current = current.Next;
+                }
+                return result;
+            }
+
+            public Node Find(int key)
+            {
+                if (count == 0)
+                {
+                    return null;
+                }
+
+                Node current = Head;
+                while (current.Value != key)
+                {
+                    current = current.Next;
+                    if (current == null)
+                    {
+                        return null;
+                    }
+                }
+
+                return current;
+            }
+
+            public void PushFront(int item)
+            {
+                Node newNode = new Node(item);
+
+                if (count != 0)
+                {
+                    newNode.Next = Head;
+                }
+                else
+                {
+                    Tail = newNode;
+                }
+
+                Head = newNode;
+                count++;
+            }
+
+            public void PushBack(int item)
+            {
+                Node newNode = new Node(item);
+                if (count == 0)
+                {
+                    Head = newNode;
+                }
+                else
+                {
+                    Tail.Next = newNode;
+                }
+
+                count++;
+                Tail = newNode;
+            }
+
+            public void AddAfter(Node node, int item)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+                Node newNode = new Node(item);
+
+                newNode.Next = node.Next;
+                node.Next = newNode;
+                if (node == Tail)
+                {
+                    Tail = newNode;
+                }
+
+                count++;
+            }
+
+            public void RemoveFirst()
+            {
+                if (count == 0)
+                {
+                    throw new Exception("Список пуст");
+                }
+
+                if (count == 1)
+                {
+                    Head = null;
+                    Tail = null;
+                }
+                else
+                {
+                    Head = Head.Next;
+                }
+
+                count--;
+            }
+            public void RemoveNode(Node node)
+            {
+                if (node == Head)
+                {
+                    RemoveFirst();
+                }
+                else
+                {
+                    Node current = Head;
+                    while (current.Next != null)
+                    {
+                        if (current.Next == node)
+                        {
+                            break;
+                        }
+
+                        current = current.Next;
+                    }
+
+                    current.Next = node.Next;
+
+                    if (node == Tail)
+                    {
+                        Tail = current;
+                    }
+
+                    count--;
+                }
             }
         }
     }
